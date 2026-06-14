bd61b21a9cbc05bd587535c7cb0a20bb0a907892
mbamonarchii17-beep <mbamonarchii17@gmail.com>
2026-06-14 20:50:17 +0500

    Backend

---
+++ server.js
@@ -0,0 +1,6 @@
+# Anthropic API kalitingizni shu yerga yozing
+# Bu faylni hech qachon GitHub ga push qilmang!
+ANTHROPIC_API_KEY=sk-ant-api03-...
+
+# Server porti (Render.com avtomatik o'rnatadi)
+PORT=3000
